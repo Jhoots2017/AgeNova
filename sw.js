7242ac43b8a33f475585780c0efe990f69c23959
Jhoots2017 <caydanjhooty@gmail.com>
2026-06-07 20:17:03 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'agenova-v16';
+const CACHE = 'agenova-v26';
 const ASSETS = ['./', 'index.html', 'manifest.webmanifest', 'icon-192.png', 'icon-512.png', 'icon-180.png'];
 
 self.addEventListener('install', e => {
